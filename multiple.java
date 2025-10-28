7e14dad0d80b9e7007ca508735334d944d3ea1ed
saniya116 <shaiksaniya6865@gmail.com>
2025-10-28 18:43:53 +0530

    multiplication.java
    This program prompts the user to enter a number and prints its multiplication table from 1 to 10.

---
+++ multiple.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
+
+output:
+Enter a number:4
+4*1=4
+4*2=8
+4*3=12
+4*4=16
+4*5=20
+4*6=24
+4*7=28
+4*8=32
+4*9=36
+4*10=40
