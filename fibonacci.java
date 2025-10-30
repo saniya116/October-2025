85b9bc7e4578612b9bad829281caed2c32531058
saniya116 <shaiksaniya6865@gmail.com>
2025-10-30 18:44:06 +0530

    Fibonacci.Java

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
+
+output:
+Enter the number of terms:7
+Fibonacci Series: 0 1 1 2 3 5 8
