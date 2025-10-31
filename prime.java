bd9573399b164448a29503610e7ffbe2d4a6376d
saniya116 <shaiksaniya6865@gmail.com>
2025-10-31 18:26:35 +0530

    prime.java

---
+++ prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) isPrime = false;
+        else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a Prime number.");
+        else
+            System.out.println(num + " is not a Prime number.");
+    }
+}
+
+output:
+Enter a number:7
+7 is a prime number
